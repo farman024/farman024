82e93e0a3264bcc32510949a182792925e42ebbc
farman024 <farman024@users.noreply.github.com>
2026-08-04 23:07:51 +0530

    Remove inspection scratch files

--- inspect3.py
+++
@@ -1,7 +0,0 @@
-import re, collections
-t = open("../efec3e8_dark.svg", "rb").read().decode("utf-16")
-print(len(t.encode()) // 1024, "KB")
-print(collections.Counter(re.findall(r'class="(\w+)"', t)).most_common(10))
-v = re.findall(r'type="translate" values="([^"]+)"', t)
-print("translate anims:", len(v))
-print("distinct:", len(set(v)))
